44cafb33116476e04db05e1e28f78db9284ac545
Nadolny <nadolnyjoaovitor@gmail.com>
2026-07-19 14:14:48 -0300

    A janela do programa já existe e botoes mostram cada área que fo idefinida

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
 import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.JButton;
+
 public class Main {
 
     public static void main(String[] args) {
-        JFrame janela = new JFrame();
+        JFrame janela = new JFrame("Gerenciador de tarefas do João!");
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setSize(500, 700);
+        janela.setLayout(new BorderLayout());
+        JButton btnNorte = new JButton("Norte");
+        btnNorte.setPreferredSize(new Dimension(100, 80));
+        JButton btnCentro = new JButton("Centro");
+        JButton btnSul = new JButton("Norte");
+        btnNorte.setPreferredSize(new Dimension(100, 80));
         janela.setVisible(true);
-
+        janela.add(btnCentro, BorderLayout.CENTER);
+        janela.add(btnNorte, BorderLayout.NORTH);
     }
 }
